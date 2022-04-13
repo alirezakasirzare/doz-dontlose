2361045a8ac7b6212a68f8fec6090e802c6b7383
Alireza Kasir <alirezakasir@yahoo.com>
2022-04-13 14:29:41 +0430

    add routes of app

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+// views
 import HomeView from "../views/HomeView.vue";
-
+import EndView from "../views/EndView.vue";
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/endofgame",
+    name: "end",
+    component: EndView,
   },
 ];
 
---
+++ src/views/EndView.vue
@@ -0,0 +1,1 @@
+<template>this is ending game handeler</template>
