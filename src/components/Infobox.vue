771fd0c2bc17b90b6abdb1ac399144b8c1e270d2
Alireza Kasir <alirezakasir@yahoo.com>
2022-04-13 20:53:39 +0430

    refactor the sass components

--- src/components/Infobox.vue
+++ src/components/Infobox.vue
@@ -1,40 +1,20 @@
 <template>
   <!-- info box -->
-  <div class="infobox">
+  <div class="listgroup">
     <!-- turn -->
-    <div class="infobox-item">
+    <div class="listgroup-item">
       <span>نوبت :</span>
       <span>من</span>
     </div>
     <!-- my score -->
-    <div class="infobox-item">
+    <div class="listgroup-item">
       <span>امتیاز من :</span>
       <span>12</span>
     </div>
     <!-- enemy score -->
-    <div class="infobox-item">
+    <div class="listgroup-item">
       <span> امتیاز رقیب :</span>
       <span>44</span>
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-// info box styles
-.infobox {
-  position: fixed;
-  top: 20px;
-  right: 20px;
-  background-color: #2196f3;
-  color: #fff;
-  min-width: 200px;
-  border-radius: 4px;
-
-  // item of info box
-  &-item {
-    padding: 10px;
-    font-size: 17px;
-    border-bottom: 1px solid rgba(238, 238, 238, 0.173);
-  }
-}
-</style>
